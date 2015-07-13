8ed33d2fdff75813144b480e2881e2bd95e8b445
khuston <khuston@silverfish.local>
2015-07-13 13:44:37 -0400

    Added dimensional variables and documentation

--- AnalyticSlabDiffusion.py
+++ AnalyticSlabDiffusion.py
@@ -9,6 +9,26 @@
 # It is a spectral solution arising from non-dimensionalization, separation of
 # variables into two Sturm-Liouville problems coupled by an eigenvalue, and
 # application of the dimensionless boundary conditions and initial conditions.
+#
+#  (A) Neumann boundary condition at x = 0
+#  D (dc/dx)|0 = h(c(0) - c_inf)
+#
+#  (B) Dirichlet boundary condition at x = L
+#  c(L) = c_L
+#
+#  (C) Governing equation
+#  dc/dt = D d^2c/dx^2
+#  \frac{\partial c}{\partial t} = D \frac{\partial^2 c}{\partial x^2}
+#
+#  Schematic of steady-state solution to diffusion with large Biot number 
+#                
+#                        (A)        (C)      (B)
+#                         |                 //|
+#                         |             //////|
+#             c_inf  c(0) |        ///////////| c(L)
+#                         |    ///////////////|
+#                         |///////////////////|
+#                       x = 0               x = L
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,7 +37,7 @@ from scipy import optimize
 
 # x is dimensionless position (Position) / (Length)
 # t is dimensionless time (Diffusion coefficient * time) / (Length ** 2)
-# Bi is the Biot number (Tranfser coefficient * Length) / (Diffusion coefficient ** 2)
+# Bi is the Biot number (Tranfser coefficient * Length) / (Diffusion coefficient)
 #  Note: setting Bi number to infinity (e.g. with np.inf) makes transfer at the x=0
 #        boundary instantaneous, and therefore makes the x=0 temperature/concentration
 #        constant
@@ -57,14 +77,28 @@ def Theta(x,t,Bi):
     return Theta_ss(x,Bi)+Theta_t(x,t,Bi)
 
 # ------------------------ #
-# Plot solutions
-Bi = np.inf
-eigv = [optimize.newton(eigv_residual, x0=k, args=(k,Bi)) for k in range(0,1000)]
-t = 0.
-x = np.linspace(0.,1.,100)
+# Plot solution
 
-for t in np.logspace(-3,1):
-    plt.plot(x,Theta(x,t,Bi))
-    plt.xlim([0,1])
-    plt.ylim([0,1])
+# Adjustable parameters
+L = 0.02                      # cm            Slab width
+D = 9e-7                      # cm**2/s       Diffusion coefficient
+h = np.inf                    # cm/s          Mass transfer coefficient at x=0 wall
+c_inf = 0.                    # mole/cm**3    Concentration at x=-infinity
+c_L = 2.                      # mole/cm**3    Concentration at x=L
+
+# Create spatial and temporal variables 
+x = np.linspace(0.,L,100)
+t = np.logspace(-3,2)
+
+# Create dimensionless variables and eigenvalues
+Bi = h*L/D
+eigv = [optimize.newton(eigv_residual, x0=k, args=(k,Bi)) for k in range(0,1000)]
+x_dimless = x/L
+t_dimless = t*D/L**2
+
+# Plot solution at each time
+for each_t in t_dimless:
+    plt.plot(x,Theta(x_dimless,each_t,Bi)*(c_L-c_inf)+c_inf)
+    plt.xlim([0,L])
+    plt.ylim([c_inf,c_L])
 plt.show()
